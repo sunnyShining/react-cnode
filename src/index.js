cc42c6cc8549dff2ca1db041c6c89e54abbbc4f4
sunshine <17765293970@163.com>
2018-01-13 23:25:52 +0800

    完善详情

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './assets/css/reset.css';
 import './assets/css/bootstrap.min.css';
 import './assets/css/style.less';
 import './assets/css/responsive.css';
+import './assets/css/font-awesome.less';
 import App from './routes/App/App.jsx';
 import registerServiceWorker from './utils/registerServiceWorker';
 import store from './redux/store/index';
--- src/routes/Topic/Topic.jsx
+++ src/routes/Topic/Topic.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
-// import classnames from 'classnames';
+import classnames from 'classnames';
 import * as topic from '../../redux/actions/topic';
 import { fromNow } from '../../utils/utils';
 
@@ -82,6 +82,47 @@ class Topic extends Component {
                             })()} />
                         </div>
                     </div>
+                </div>
+                <div className="panel">
+                    <div className="header">
+                        <span className="col_fade">11 回复</span>
+                    </div>
+                    {
+                        topic.replies ? topic.replies.map((item, index) => {
+                            return (
+                                <div key={index} className={classnames('cell', 'reply_area', 'reply_item', {'reply_highlight': item.is_uped})} reply_id="5a54a935a89c475d7ea4fb54" reply_to_id="" id="5a54a935a89c475d7ea4fb54">
+                                    <div className="author_content">
+                                        <a href="/user/i5ting" className="user_avatar">
+                                            <img src={item.author.avatar_url} title="i5ting" />
+                                        </a>
+                                        <div className="user_info">
+                                            <a className="dark reply_author" href="/user/i5ting">{item.author.loginname}</a>
+                                            <a className="reply_time" href="#5a54a935a89c475d7ea4fb54">{index+1}楼•{fromNow(item.create_at)}</a>
+                                            <span className="reply_by_author">作者</span>
+                                        </div>
+                                        <div className="user_action">
+                                            <span>
+                                                <i className="fa up_btn fa-thumbs-o-up" title="喜欢"></i>
+                                                <span className="up-count">
+                                                  &nbsp;{item.ups ? item.ups.length : 0}
+                                                </span>
+                                            </span>
+                                            <span> </span>
+                                        </div>
+                                    </div>
+                                    <div className="reply_content from-i5ting">
+                                        <div dangerouslySetInnerHTML={(() => {
+                                            return {__html: item.content};
+                                        })()} />
+                                    </div>
+                                    <div className="clearfix">
+                                        <div className="reply2_area">
+                                        </div>
+                                    </div>
+                                </div>
+                            );
+                        }) : null
+                    }
                 </div>
             </div>
         );
