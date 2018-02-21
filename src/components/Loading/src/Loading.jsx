b8a3da23eea1b0aaf15a79283369f4fa941d62d8
sunshine <17765293970@163.com>
2018-02-22 00:01:25 +0800

    eslint警告

--- src/components/Loading/src/Loading.jsx
+++ src/components/Loading/src/Loading.jsx
@@ -7,7 +7,7 @@
 */
 
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import './Loading.less';
 
